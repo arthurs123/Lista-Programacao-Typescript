7a7978ef5f518dacfdcf8e95fc9b46a9fc49166e
arthurs123 <arthuroaraujo9@gmail.com>
2026-09-09 08:22:43 -0300

    new quests

---
+++ src/POO/q6.ts
@@ -0,0 +1,29 @@
+// 7. Uma empresa quer cadastrar funcionários e aplicar aumento salarial. O sistema deve pedir nome,
+// cargo e salário. Crie um método que receba um percentual de aumento e atualize o salário do
+// funcionário, exibindo o seu nome e novo valor.
+
+
+class Cadastro{
+
+    nome:string
+    cargo:string
+    private _salario:number
+
+    constructor(name:string, position:string, wage:number){
+       this.nome=name
+       this.cargo=position
+       this._salario=wage
+    }
+    
+    percentual(){
+        
+    }
+
+
+
+
+
+
+
+
+}
